2b728e8407d50b89465ca9b6f7b674de38a21d6f
Ajay A Adsule <103304466+AjayAdsule@users.noreply.github.com>
2023-02-09 14:43:07 +0530

    first commit

--- index.js
+++ index.js
@@ -12,4 +12,27 @@ function te(){
 let set_a=new Set()
 set_a=["ajay","vijay"]
 
-console.log(typeof set_a)
+// console.log(typeof set_a)
+
+class person{
+    constructor(name,mob_no,dob,yop){
+        this.name=name;
+        this.mob_no=mob_no;
+        this.dob=dob;
+        this.yop=yop;
+        this.result=function(){
+            console.log(`My name is ${this.name}`)
+            console.log(`my date of birth is ${this.dob}`)
+            console.log(`my mobile number is ${mob_no}`)
+            console.log(`my year of passing is ${this.yop}`)
+        }
+    }
+}
+let ajay=new person("ajay",1999,7977223876,2021)
+// ajay.result();
+
+let arr=["ajay","vijay",["mohit","rohit",12,85]]
+arr.push(["dinesh"])
+arr.pop()
+arr.sort()
+console.log(arr)
